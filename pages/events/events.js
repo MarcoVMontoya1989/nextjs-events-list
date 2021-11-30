7eed9c45577523f4d5db737dda51b58230ae6081
Marco Antonio V Montoya <marcovmontoya@gmail.com>
2021-11-29 22:06:45 -0500

    feat: add starters pages and dummy data

---
+++ pages/events/events.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+import {useRouter} from "next/router";
+
+const Events = () => {
+  return (
+    <div>
+      <h1>all events</h1>
+    </div>
+  );
+};
+
+export default Events;
