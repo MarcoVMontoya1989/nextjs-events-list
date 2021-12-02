8978ebd6dcbcd85f7b74852b7a25d653e4d26640
Marco Antonio V Montoya <marcovmontoya@gmail.com>
2021-12-01 21:37:07 -0500

    bug: correction to show correct data

--- components/events/EventDetail/event-logistics.js
+++ components/events/EventDetail/event-logistics.js
@@ -4,19 +4,20 @@ import LogisticsItem from './logistics-item';
 import classes from '../../../styles/Components/EventLogistics.module.scss';
 
 function EventLogistics(props) {
-  const { date, address, image, imageAlt } = props;
+
+  const { date, location, image, title } = props.item;
 
   const humanReadableDate = new Date(date).toLocaleDateString('en-US', {
     day: 'numeric',
     month: 'long',
     year: 'numeric',
   });
-  const addressText = address.replace(', ', '\n');
+  const addressText = location.replace(', ', '\n');
 
   return (
     <section className={classes.logistics}>
       <div className={classes.image}>
-        <img src={`/${image}`} alt={imageAlt} />
+        <img src={`/${image}`} alt={title} />
       </div>
       <ul className={classes.list}>
         <LogisticsItem icon={DateIcon}>
--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -2,24 +2,32 @@ import {Fragment} from "react";
 import {useRouter} from "next/router";
 import {getEventById} from "../../dummy-data";
 import EventItemComponent from "../../components/events/EventItem.component";
-import Error404 from "../404";
 import EventSummary from "../../components/events/EventDetail/event-summary";
 import EventLogistics from "../../components/events/EventDetail/event-logistics";
 import EventContent from "../../components/events/EventDetail/event-content";
+import ErrorAlert from "../../components/events/ErrorAlert/error-alert";
 
-const EventId = (props) => {
-
+const EventId = () => {
   const router = useRouter();
   const eventId = router.query.eventId;
   const eventGet = getEventById(eventId);
 
+  console.log('routers', router.query);
+  console.log('get item', eventGet);
+
+  const eventCardContent = (
+    <Fragment>
+      <EventSummary title={eventGet.title} />
+      <EventLogistics item={eventGet} />
+      <EventContent>
+        {eventGet.description}
+      </EventContent>
+    </Fragment>
+  );
+
   return (
     <Fragment>
-      {eventGet ? <EventItemComponent item={eventGet} /> : <Error404 />}
-      <EventSummary />
-      <EventContent>
-
-      </EventContent>
+      {eventGet ? eventCardContent : <ErrorAlert /> }
     </Fragment>
   );
 };
